7f9e30efde0314cef844343652fcfb26cc57a747
mrtunluer <mertnler113@gmail.com>
2022-08-15 22:44:44 +0300

    Add Axis Right Chart

--- app/src/main/java/com/mertdev/weighttracking/ui/home/chart/ChartStyle.kt
+++ app/src/main/java/com/mertdev/weighttracking/ui/home/chart/ChartStyle.kt
@@ -3,23 +3,21 @@ package com.mertdev.weighttracking.ui.home.chart
 import android.content.Context
 import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.LineDataSet
 import com.mertdev.weighttracking.R
 
 object ChartStyle {
 
     fun styleChart(lineChart: LineChart) = lineChart.apply {
-        axisRight.isEnabled = false
         xAxis.isEnabled = false
 
         axisLeft.apply {
-            textColor = ContextCompat.getColor(context, R.color.white)
-            textSize = context.resources.getDimension(R.dimen.chartTextSize)
-            isGranularityEnabled = false
-            setDrawAxisLine(true)
-            setDrawGridLines(false)
-            axisLineColor = ContextCompat.getColor(context, R.color.blue)
-            axisLineWidth = context.resources.getDimension(R.dimen.chartLineWidth)
+            initAxis(this, context)
+        }
+
+        axisRight.apply {
+            initAxis(this, context)
         }
 
         setTouchEnabled(true)
@@ -44,6 +42,16 @@ object ChartStyle {
         isHighlightEnabled = true
         lineWidth = context.resources.getDimension(R.dimen.chartLineWidth)
         mode = LineDataSet.Mode.HORIZONTAL_BEZIER
+    }
+
+    private fun initAxis(axis: YAxis, context: Context) = with(axis){
+        textColor = ContextCompat.getColor(context, R.color.white)
+        textSize = context.resources.getDimension(R.dimen.chartTextSize)
+        isGranularityEnabled = false
+        setDrawAxisLine(true)
+        setDrawGridLines(false)
+        axisLineColor = ContextCompat.getColor(context, R.color.blue)
+        axisLineWidth = context.resources.getDimension(R.dimen.chartLineWidth)
     }
 
 }
