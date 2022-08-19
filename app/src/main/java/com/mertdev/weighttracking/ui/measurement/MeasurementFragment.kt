ab267602f3de4e50ec6ca142bf0200a3388825dc
mrtunluer <mertnler113@gmail.com>
2022-08-20 01:39:38 +0300

    add measurement fragment

---
+++ app/src/main/java/com/mertdev/weighttracking/ui/measurement/MeasurementFragment.kt
@@ -0,0 +1,19 @@
+package com.mertdev.weighttracking.ui.measurement
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.viewbinding.library.fragment.viewBinding
+import com.mertdev.weighttracking.R
+import com.mertdev.weighttracking.databinding.FragmentMeasurementBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MeasurementFragment : Fragment(R.layout.fragment_measurement) {
+    private val binding: FragmentMeasurementBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
