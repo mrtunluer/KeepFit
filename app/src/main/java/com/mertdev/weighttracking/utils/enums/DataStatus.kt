8c7001b3a647cb1f8d265475d48a33196e3ada47
mrtunluer <mertnler113@gmail.com>
2022-11-28 13:47:12 +0300

    reformat file

--- app/src/main/java/com/mertdev/weighttracking/utils/enums/DataStatus.kt
+++ app/src/main/java/com/mertdev/weighttracking/utils/enums/DataStatus.kt
@@ -1,14 +1,13 @@
 package com.mertdev.weighttracking.utils.enums
 
 sealed class DataStatus<T>(
-    val status: Status, val data: T? = null, val message: String? = null) {
-    class Loading<T>(data: T? = null): DataStatus<T>(Status.LOADING,data)
-    class Error<T>(message: String?,data: T? = null): DataStatus<T>(Status.ERROR,data, message)
-    class Success<T>(data: T?): DataStatus<T>(Status.SUCCESS,data)
+    val status: Status, val data: T? = null, val message: String? = null
+) {
+    class Loading<T>(data: T? = null) : DataStatus<T>(Status.LOADING, data)
+    class Error<T>(message: String?, data: T? = null) : DataStatus<T>(Status.ERROR, data, message)
+    class Success<T>(data: T?) : DataStatus<T>(Status.SUCCESS, data)
 }
 
-enum class Status{
-    SUCCESS,
-    LOADING,
-    ERROR
+enum class Status {
+    SUCCESS, LOADING, ERROR
 }
